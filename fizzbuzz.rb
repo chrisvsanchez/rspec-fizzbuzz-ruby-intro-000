0536ac57acb4fede4ee2624e2031b9d5ab7a40c6
Chris Sanchez <chrisvsanchez@gmail.com>
2020-02-12 02:33:14 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -10,3 +10,16 @@ def fizzbuzz(num)
   else 
  end 
  end
+
+def fizzbuzz(num)
+  if ((num % 3 == 0 && num % 5 == 0))
+    return "FizzBuzz"
+  elsif (num % 5 == 0)
+    return "Buzz"
+  elsif (num % 3 == 0 && num % 5 == 0)
+    return "FizzBuzz"
+  elsif(num % 3 == 0 || num % 5 == 0)
+    return nil
+  else 
+ end 
+ end
