5f37556553569e92214e8619bbe50f7c6fb7f2a6
Chris Sanchez <chrisvsanchez@gmail.com>
2020-02-12 02:28:42 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,7 +2,6 @@
 require_relative '../fizzbuzz.rb'
 require_relative './spec_helper.rb'
 
-
 describe "fizzbuzz" do
   it 'returns "Fizz" when the number is divisible by 3' do
     fizz_3 = fizzbuzz(3)
